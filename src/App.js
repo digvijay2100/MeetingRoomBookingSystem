a268cefb418c5e171b9a15a78819f1bc61b86496
Digvijay Mahadik <113425336+digvijay2100@users.noreply.github.com>
2024-07-25 17:25:24 +0530

    commit
    update and delete

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
   const [end, setEnd] = useState('');
   const [title, setTitle] = useState('');
   const [data, setData] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
 
   function updateValue(room) {
     console.log(room);
@@ -24,34 +26,75 @@ function App() {
       setBoard((prevBoard) => prevBoard - 1);
     }
 
-    setData([
-      ...data,
-      {
+    if (isEdit) {
+      const updatedData = [...data];
+      updatedData[editIndex] = {
         room,
         date,
         start,
         end,
         title,
-      },
-    ]);
-    
-
+      };
+      setData(updatedData);
+      setIsEdit(false);
+      setEditIndex(null);
+    } else {
+      setData([
+        ...data,
+        {
+          room,
+          date,
+          start,
+          end,
+          title,
+        },
+      ]);
+    }
   }
 
   const handleSubmit = (e) => {
+    //const formRef = React.createRef();
     e.preventDefault();
     updateValue(room);
-    
-    
-    
+   // formRef.current.reset();
+    setRoom('');
+    setDate('');
+    setStart('');
+    setEnd('');
+    setTitle('');  
+  };
 
-    
+  const handleEdit = (index) => {
+    const editData = data[index];
+    setRoom(editData.room);
+    setDate(editData.date);
+    setStart(editData.start);
+    setEnd(editData.end);
+    setTitle(editData.title);
+    setIsEdit(true);
+    setEditIndex(index);
+  };
+
+  const handleDelete = (index) => {
+    const deletedData = data[index];
+    if (deletedData.room === 'Conference Room A') {
+      setConf((prevConf) => prevConf + 1);
+    } else if (deletedData.room === 'Meeting Room B') {
+      setMeet((prevMeet) => prevMeet + 1);
+    } else {
+      setBoard((prevBoard) => prevBoard + 1);
+    }
+    const updatedData = [...data];
+    updatedData.splice(index, 1);
+    setData(updatedData);
   };
 
   return (
     <>
-      <div className='header'>
-        <center><h1> Meeting Room Booking System</h1></center>
+      <div className="header">
+        <center>
+          <h1>Meeting Room Booking System</h1>
+        </center>
       </div>
       <div id="main">
         <div className="div">
@@ -62,11 +105,11 @@ function App() {
         </div>
 
         <div className="div">
-          <form onSubmit={handleSubmit} id='form'>
-          <h1>Book a Room</h1>
+          <form  onSubmit={handleSubmit} id="form">
+            <h1>Book a Room</h1>
             <h3>Room:</h3>
-            <select onChange={(e) => setRoom(e.target.value)} required>
-              <option value="">Select a Room</option>
+            <select onChange={(e) => setRoom(e.target.value) } required>
+              <option value="Select a Room">Select a Room</option>
               <option value="Conference Room A">Conference Room A</option>
               <option value="Meeting Room B">Meeting Room B</option>
               <option value="Board Room">Board Room</option>
@@ -75,57 +118,73 @@ function App() {
             <h3>Date:</h3>
             <input
               type="date"
+              value={date}
+              
               id="date"
               onChange={(e) => setDate(e.target.value)}
               required
-            ></input>
+            />
             <h3>Start Time:</h3>
             <input
               type="time"
+              value={start}
+
               id="start"
               onChange={(e) => setStart(e.target.value)}
               required
-            ></input>
+            />
             <h3>End Time:</h3>
             <input
               type="time"
+              value={end}
               id="end"
               onChange={(e) => setEnd(e.target.value)}
               required
-            ></input>
+            />
             <h3>Meeting Title:</h3>
             <input
               type="text"
+              value={title}
               id="title"
               onChange={(e) => setTitle(e.target.value)}
               required
-            ></input>
+            />
             <br />
-            <button type="submit">Book Room</button>
+            <button type="submit">
+              {isEdit ? "Update Booking" : "Book Room"}
+            </button>
           </form>
         </div>
       </div>
       <center>
+        
         <div id="navbar">
           <h1>Booking Management</h1>
           <table border={1} className="table">
-            <thead>
+              <thead>
               <tr>
+              <th>Index</th>
                 <th>Room</th>
                 <th>Date</th>
                 <th>Start Time</th>
                 <th>End Time</th>
                 <th>Meeting Title</th>
+                <th>Update</th>
+                <th>Delete</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody id='content'>
               {data.map((booking, index) => (
-                <tr key={index}>
+                <tr key={index }>
+                
+                <td>{index +1}</td>
                   <td>{booking.room}</td>
                   <td>{booking.date}</td>
                   <td>{booking.start}</td>
                   <td>{booking.end}</td>
                   <td>{booking.title}</td>
+                  <td ><button onClick={()=>handleEdit(index)} id='update'>Update</button></td>
+                  <td><button onClick={()=>handleDelete(index)} id='delete'>Delete</button></td>
                 </tr>
               ))}
             </tbody>
